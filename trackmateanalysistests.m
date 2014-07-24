bfcaaac578c9bd972caff9ba671e43b2fa27dd68
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-07-24 13:14:50 -0400

    Calculating MSD
    Wasn't that easy?

--- trackmateanalysistests.m
+++ trackmateanalysistests.m
@@ -1,10 +1,20 @@
+clf
+
+
 [tracks, md] = importTrackMateTracks('F:\July18201421_Tracks.xml', 'ClipZ');
 
 
 % Instantiate the MSD analyzer
 
-ma = msdanalyzer(2, md.spaceUnits, md.timeUnits); % 2-dimensions
+msda = msdanalyzer(2, md.spaceUnits, md.timeUnits); % 2-dimensions
 
-ma = ma.addAll(tracks);
+msda = msda.addAll(tracks);
 
-disp(ma)
+disp(msda)
+
+msda.plotTracks()
+msda.labelPlotTracks() % This automatically labels the graph axes
+
+msda = msda.computeMSD();
+
+msda.msd()
