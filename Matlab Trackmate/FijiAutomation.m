cc817ca2fec4d29d53cf08d511841f953629fd20
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-08-01 10:16:09 -0400

    Framework for MATLAB/FIJI automation

---
+++ Matlab Trackmate/FijiAutomation.m
@@ -0,0 +1,27 @@
+Miji;   % Starts a FIJI inside MATLAB
+
+%% Select Images to Process %%
+
+[FileName,PathName,FilterIndex] = uigetfile(FilterSpec,DialogTitle, 'MultiSelect', 'on')
+
+%% Open Image %%
+
+
+%% Local Contrast %%
+
+
+%% LUT Changes %%
+
+
+%% Other Processing %%
+
+
+%% Save LUT Changes %%
+
+
+
+%% Tracking %%
+
+
+
+%% Save Tracking Results %%
